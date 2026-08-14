5531da2f6dc4740c93d6a2e53ce47b70567ee25d
lopesllm <lucaslopesmoreira@gmail.com>
2026-08-13 21:27:45 -0300

    Exercicio 3 e Resposta 3

--- resposta2.php
+++ resposta2.php
@@ -1,5 +1,5 @@
 <?php
     $valor1 = $_POST["valor1"];
     $valor2 = $_POST["valor2"];
-    $subtração = $valor1 - $valor2;
-    echo "O valor da subtração é $subtração";
+    $subtracao = $valor1 - $valor2;
+    echo "O valor da subtração é $subtracao";
